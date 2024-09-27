81be47d5c50e75e066bb27fa0c398a7c35d7897e
Rizki Damar Arum <damararum06@gmail.com>
2024-09-27 09:50:49 +0700

    damar

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,7 @@
 public class HelloWord {
     public static void main(String[] args) {
       System.out.println("HelloWorld"); 
+      System.out.println("Haloo Guys Nama Saya");
+      System.out.println("Damar");
     }
 }
